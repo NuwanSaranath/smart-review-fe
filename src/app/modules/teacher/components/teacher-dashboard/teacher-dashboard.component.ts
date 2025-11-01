5da62982a2034dadf470885c14771845ad2ee1bd
Nuwanssu <nuwanssu@cf.lk>
2025-11-01 09:14:04 +0530

    changes

--- src/app/modules/teacher/components/teacher-dashboard/teacher-dashboard.component.ts
+++ src/app/modules/teacher/components/teacher-dashboard/teacher-dashboard.component.ts
@@ -20,18 +20,15 @@ export class TeacherDashboardComponent {
   name = 'Mr. Nuwan Saranath';
 
   metrics: Metric[] = [
-    { label: 'Completion', value: '72%' },
-    { label: 'Overdue reviews', value: '14' },
+
     { label: 'Total Classes', value: '3' },
-    { label: 'Total Students', value: '86' }
+    { label: 'Total Students', value: '4' }
   ];
 
   glance: GlanceItem[] = [
-    { icon:'class',      title:'CS101 - Intro to CS',              sub:'10:00 AM - 11:00 AM', actionLabel:'View' },
-    { icon:'class',      title:'ENG110 - Academic Writing',        sub:'1:00 PM - 2:00 PM',   actionLabel:'View' },
-    { icon:'assignment', title:'Assignment 1 - Intro to CS',       sub:'Due: Sep 15',         actionLabel:'View' },
-    { icon:'assignment', title:'Assignment 2 - Calculus II',       sub:'Due: Sep 10',         actionLabel:'View' },
-    { icon:'assignment', title:'Assignment 3 - Academic Writing',  sub:'Due: Sep 5',          actionLabel:'View' },
-    { icon:'bookmark',   title:'Topics to review',                 sub:'View all',            actionLabel:'View' }
+
+    { icon:'assignment', title:'Assignment 1 - Set Theory',        sub:'Due: Sep 15',          },
+    { icon:'assignment', title:'Assignment 2 - Calculus II',       sub:'Due: Sep 10',         },
+    { icon:'assignment', title:'Assignment 3',  sub:'Due: Sep 5',           }
   ];
 }
